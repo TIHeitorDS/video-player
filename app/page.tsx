fd5df8ced64ce027e77e2f408dba13bf17cf1570
TIHeitorDS <heitorclaudinods@gmail.com>
2025-11-19 07:54:15 -0300

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,63 +1,73 @@
-import Image from "next/image";
+"use client";
+
+import { useRef, useState } from "react";
+
+import { Play } from "lucide-react";
+import { Pause } from "lucide-react";
+import { Volume2 } from "lucide-react";
 
 export default function Home() {
+  const videoRef = useRef<HTMLVideoElement>(null);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [volume, setVolume] = useState(0.5);
+
+  const handlePlay = () => {
+    if (!videoRef.current) return;
+
+    if (isPlaying) {
+      videoRef.current.pause();
+      setIsPlaying(false);
+    } else {
+      videoRef.current.play();
+      setIsPlaying(true);
+    }
+  };
+
+  const handleVolumeChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (!videoRef.current) return;
+
+    setVolume(parseFloat(e.target.value));
+
+    videoRef.current.volume = volume;
+  };
+
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <div className="h-screen bg-black flex items-start pt-6 lg:items-center justify-center">
+      <main className="lg:h-3/4 w-11/12 rounded-lg overflow-hidden relative group bg-[#1e1e1e]">
+        <video ref={videoRef} className="w-full h-full">
+          <source src="video.mp4" type="video/mp4" />
+        </video>
+
+        <div className="absolute top-0 z-50 h-12 px-6 group-hover:opacity-100 opacity-0 transition-opacity duration-200 bg-black/30 w-full flex items-center">
+          <p className="font-bold text-white text-xl tracking-wide">BLEACH</p>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+
+        <div className="flex justify-between items-center bg-black/30 lg:opacity-0 lg:group-hover:opacity-100 transition-opacity duration-200 w-full absolute z-50 bottom-0 h-12 px-6">
+          <button
+            onClick={handlePlay}
+            type="button"
+            className=" h-6.5 w-6.5 cursor-pointer"
+            tabIndex={-1}
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
+            {isPlaying ? <Pause color="#39FF14" /> : <Play color="#39FF14" />}
+          </button>
+
+          <div className="flex items-center gap-x-3.5">
+            <button type="button">
+              <Volume2 color="#39FF14" />
+            </button>
+
+            <input
+              type="range"
+              name="volume"
+              id="volume"
+              min="0"
+              max="1"
+              value={volume}
+              step="0.01"
+              onChange={handleVolumeChange}
             />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+          </div>
         </div>
       </main>
     </div>
